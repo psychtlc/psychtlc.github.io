a1a0acd2e853175fb189cba991988612fded9291
Ryan Glover <ersatzryan@gmail.com>
2026-02-21 20:02:01 -0500

    chore(dev): Add devenv to manage dev environment

---
+++ devenv.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+  packages = with pkgs; [
+    git
+  ];
+
+  # https://devenv.sh/languages/
+  languages.ruby = {
+    enable = true;
+    bundler.enable = true;
+  };
+
+  processes.dev.exec = "jekyll server --watch";
+}
